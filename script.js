6e17fedc681862c9083f7a5a7bb0c11637a37964
SarikViper <dzamolovsardor5@gmail.com>
2025-01-31 21:13:08 +0500

    added function

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ closeButton.onclick = () => {
     modalWindow.classList.add("modal-dis_active");
 };
 
+
 document.onclick =(e) => {
     if(!modalWindow.contains(e.target) && e.target != modalButton) {
         modalWindow.classList.remove("modal-active");
         modalWindow.classList.add("modal-dis_active");
     }
-}
+};
